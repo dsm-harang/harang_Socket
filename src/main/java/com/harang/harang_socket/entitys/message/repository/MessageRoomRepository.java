01e22bd5d6072c096fdd0f97bdebb8a4015d696a
Ryua <machiro119@naver.com>
2020-11-23 16:27:03 +0900

    [FIX] - annotation repository

--- src/main/java/com/harang/harang_socket/entitys/message/repository/MessageRoomRepository.java
+++ src/main/java/com/harang/harang_socket/entitys/message/repository/MessageRoomRepository.java
@@ -2,6 +2,8 @@ package com.harang.harang_socket.entitys.message.repository;
 
 import com.harang.harang_socket.entitys.message.MessageRoom;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MessageRoomRepository extends CrudRepository<MessageRoom, Integer> {
 }
