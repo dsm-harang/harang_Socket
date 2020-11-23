d74edefb506b7ee75986e2032a4a7d2905ea5998
Ryua <machiro119@naver.com>
2020-11-23 16:27:00 +0900

    [FIX] - annotation repository

--- src/main/java/com/harang/harang_socket/entitys/message/repository/MessageRepository.java
+++ src/main/java/com/harang/harang_socket/entitys/message/repository/MessageRepository.java
@@ -2,9 +2,11 @@ package com.harang.harang_socket.entitys.message.repository;
 
 import com.harang.harang_socket.entitys.message.Message;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface MessageRepository extends CrudRepository<Message, Integer> {
     boolean existsByRoomIdAndSenderId(String roomId, Integer senderId);
     Optional<Message> findByRoomIdAndSenderIdNot(String roomId, Integer senderId);
