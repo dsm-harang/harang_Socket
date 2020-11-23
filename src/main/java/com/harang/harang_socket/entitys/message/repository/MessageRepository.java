68469b07f72f958c3208e2a0f8951028e4b13a61
Ryua <machiro119@naver.com>
2020-11-23 15:58:24 +0900

    [FIX] - add orm

--- src/main/java/com/harang/harang_socket/entitys/message/repository/MessageRepository.java
+++ src/main/java/com/harang/harang_socket/entitys/message/repository/MessageRepository.java
@@ -3,5 +3,9 @@ package com.harang.harang_socket.entitys.message.repository;
 import com.harang.harang_socket.entitys.message.Message;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface MessageRepository extends CrudRepository<Message, Integer> {
+    boolean existsByRoomIdAndSenderId(String roomId, Integer senderId);
+    Optional<Message> findByRoomIdAndSenderIdNot(String roomId, Integer senderId);
 }
